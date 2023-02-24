6e7ed6aa4af17f799956a887510169ae0d6dc832
atapon11 <634259017@webmail.npru.ac.th>
2023-02-24 15:44:11 +0700

    atthaphon017

--- student/src/router/Router.js
+++ student/src/router/Router.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Layout from '../layout/Layout';
-import MainStudent from '../view/student/MainStudent';
-import MainHome from '../view/home/MainHome';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../layout/Layout";
+import MainStudent from "../view/student/MainStudent";
+import MainHome from "../view/home/MainHome";
+import MainTest from "../test/MainTest";
 
-function Router () {
+function Router() {
   return (
     <BrowserRouter>
-    <Layout>
-    <Routes>
-        <Route path="/" element={<MainHome/>}/>
-        <Route path="/student" element={<MainStudent/>}/>
-    </Routes>
-    </Layout>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<MainHome />} />
+          <Route path="/student" element={<MainStudent />} />
+          <Route path="/test" element={<MainTest />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
- 
   );
 }
 
